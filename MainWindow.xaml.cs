74123f814ebd071f1258e01a63dc5212a50583de
Eesan1 <166600020+Eesan1@users.noreply.github.com>
2024-09-10 22:57:45 +0200

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,25 +8,21 @@ namespace CMCS
         public MainWindow()
         {
             InitializeComponent();
-            // Load the Submit Claim view by default
             MainContent.Content = new LecturerSubmitClaim();
         }
 
         private void SubmitClaim_Click(object sender, RoutedEventArgs e)
         {
-            // Navigate to the LecturerSubmitClaim view
             MainContent.Content = new LecturerSubmitClaim();
         }
 
         private void ViewClaims_Click(object sender, RoutedEventArgs e)
         {
-            // Navigate to the LecturerClaims view
             MainContent.Content = new LecturerClaims();
         }
 
         private void LogOut_Click(object sender, RoutedEventArgs e)
         {
-            // Close the application
             Application.Current.Shutdown();
         }
     }
