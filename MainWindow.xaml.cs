7cf9383a4c7d312344960b68530d43d927f83302
Eesan1 <166600020+Eesan1@users.noreply.github.com>
2024-09-10 22:53:01 +0200

    Add files via upload
    These are the main window files which will run the home window for the GUI application

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using CMCS.Views;
+
+namespace CMCS
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Load the Submit Claim view by default
+            MainContent.Content = new LecturerSubmitClaim();
+        }
+
+        private void SubmitClaim_Click(object sender, RoutedEventArgs e)
+        {
+            // Navigate to the LecturerSubmitClaim view
+            MainContent.Content = new LecturerSubmitClaim();
+        }
+
+        private void ViewClaims_Click(object sender, RoutedEventArgs e)
+        {
+            // Navigate to the LecturerClaims view
+            MainContent.Content = new LecturerClaims();
+        }
+
+        private void LogOut_Click(object sender, RoutedEventArgs e)
+        {
+            // Close the application
+            Application.Current.Shutdown();
+        }
+    }
+}
